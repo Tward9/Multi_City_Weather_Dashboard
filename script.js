8c2415d33cb2142bc6a2223a10ccdc83c68a8132
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-03-09 14:26:38 -0600

    inputs city, logs weather for that city

---
+++ script.js
@@ -0,0 +1,24 @@
+var APIKey = '306e5a39201f9a04bf59daf2b8544d8a';
+
+$('#citySubmit').on('click', function(event){
+    city = 'Chicago'
+    event.preventDefault();
+    var newCity = $('#cityChoice').val();
+    city = newCity
+    console.log(city)
+    return city;
+})
+
+function getWeatherAPI(){
+    console.log(city);
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    console.log(queryURL);
+    fetch(queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+$('#weatherBtn').on('click', getWeatherAPI);
