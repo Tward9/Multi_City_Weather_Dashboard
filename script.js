10f5b745a76d24c73257b37b8e03a40416d22c4e
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-03-09 16:36:15 -0600

    displays list of current weather for city input.

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
 var APIKey = '306e5a39201f9a04bf59daf2b8544d8a';
 
-$('#citySubmit').on('click', function(event){
+$('#citySubmit').on('click', function (event) {
     city = 'Chicago'
     event.preventDefault();
+    $('#currentWeather').empty();
     var newCity = $('#cityChoice').val();
     city = newCity
     console.log(city)
     // return city;
     getWeatherAPI();
     getForecastAPI();
+    $('#currentWeather').append(
+        $('<li/>')
+            .attr("id", "currentCity")
+            .text(city)
+    );
 })
-function getWeatherAPI(){
+function getWeatherAPI() {
     console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     console.log(queryURL);
     fetch(queryURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.name);
+            console.log(data);
+            if (data.wind.deg <= 11 || data.wind.deg >= 349) {
+                var direction = 'N';
+            }else if (data.wind.deg >= 12 && data.wind.deg <= 33) {
+                direction = 'NNE';
+            }else if (data.wind.deg >= 34 && data.wind.deg <= 56) {
+                direction = 'NE';
+            }else if (data.wind.deg >= 57 && data.wind.deg <= 78) {
+                direction = 'ENE';
+            }else if (data.wind.deg >= 79 && data.wind.deg <= 101) {
+                direction = 'E';
+            }else if (data.wind.deg >= 102 && data.wind.deg <= 123) {
+                direction = 'ESE';
+            }else if (data.wind.deg >= 124 && data.wind.deg <= 146) {
+                direction = 'SE';
+            }else if (data.wind.deg >= 147 && data.wind.deg <= 168) {
+                direction = 'SSE';
+            }else if (data.wind.deg >= 169 && data.wind.deg <= 191) {
+                direction = 'S';
+            }else if (data.wind.deg >= 192 && data.wind.deg <= 213) {
+                direction = 'SSW';
+            }else if (data.wind.deg >= 214 && data.wind.deg <= 236) {
+                direction = 'SW';
+            }else if (data.wind.deg >= 237 && data.wind.deg <= 258) {
+                direction = 'WSW';
+            }else if (data.wind.deg >= 259 && data.wind.deg <= 281) {
+                direction = 'W';
+            }else if (data.wind.deg >= 282 && data.wind.deg <= 303) {
+                direction = 'WNW';
+            }else if (data.wind.deg >= 304 && data.wind.deg <= 326) {
+                direction = 'NW';
+            }else{
+                direction = 'NNW'
+            }
+            $('#currentWeather').append($('<li/>').text('Temp: ' + ((data.main.temp - 273.15) * 9/5 + 32).toFixed(2) + ' Degrees F'));
+            $('#currentWeather').append($('<li/>').text('Feels Like: ' + ((data.main.feels_like - 273.15) * 9/5 + 32).toFixed(2) + ' Degrees F'));
+            $('#currentWeather').append($('<li/>').text('Wind: ' + data.wind.speed + ' MPH Direction: ' + direction));
+            $('#currentWeather').append($('<li/>').text('Humidity: ' + data.main.humidity + ' %'));
+        })
 }
-// $('#weatherBtn').on('click', getWeatherAPI);
-function getForecastAPI(){
+// save for later: http://openweathermap.org/img/wn/10d@2x.png
+//weather icon. get from data.weather.icon
+//set as image link 'http://openweathermap.org/img/wn/' + data.weather.icon + '@2x.png'
+function getForecastAPI() {
     console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
     console.log(queryURL);
     fetch(queryURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                
+            }
+        })
 }
 // $('#forecastBtn').on('click', getForecastAPI);
